162bf843246107853cd7fe362c0c81d40491e676
Abhishek <1998abhi.jha.01@gmail.com>
2021-08-04 00:28:13 +0530

    To add a java code in this repository

---
+++ Mohit/1stCode.java
@@ -0,0 +1,54 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	  String name = "Abhishek";
+	  System.out.println(name);
+	}
+}
+
+/*
+
+
+name.concat("Mohit");
+name ----------------------> Abhishek
+
+name = name.concat("Mohit")
+name ----------------------> AbhishekMohit
+
+sum = 5+10
+
+----------------------
+5
+----------------------
+15
+------------------------
+sayed
+------------------------
+ali  sayed
+------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
